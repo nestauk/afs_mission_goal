da2aec896e8d6d6ea30cf9832594ae5c8eeb9dad
Simran <simranmotiani96@gmail.com>
2024-07-03 13:45:55 +0100

    Created empty ipynb

---
+++ afs_mission_goal/getters/01_simran_analysis.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     comment_magics: true
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.2
+#   kernelspec:
+#     display_name: afs_mission_goal
+#     language: python
+#     name: afs_mission_goal
+# ---
+
+# %%
+import pandas as pd
+
+# %%
