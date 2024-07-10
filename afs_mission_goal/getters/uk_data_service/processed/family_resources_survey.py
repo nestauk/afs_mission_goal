b0978f554849e2eaabe6f2a42df2d55f91eac476
Rachel <rachel.wilcock@nesta.org.uk>
2024-07-10 09:28:41 +0100

    Files for reading in the family resources survey

---
+++ afs_mission_goal/getters/uk_data_service/processed/family_resources_survey.py
@@ -0,0 +1,60 @@
+import pandas as pd
+from nesta_ds_utils.loading_saving.S3 import download_obj
+
+BUCKET = "afs-uk-data-service"
+frs_datasets = [
+    "accounts",
+    "adult",
+    "assets",
+    "benefits",
+    "benefit_unit",
+    "care",
+    "child",
+    "childcare",
+    "dictionary",
+    "endowment",
+    "ext_child",
+    "frs2223",
+    "gov_pay",
+    "household",
+    "job",
+    "maint",
+    "mort_cont",
+    "mortgage",
+    "odd_job",
+    "owner",
+    "pension_provider",
+    "pension",
+    "rent_cont",
+    "renter",
+    "tables",
+]
+
+
+def get_all_datasets() -> dict:
+    """
+    Function to load all datasets from the Family Resources Survey from the UK Data Service.
+    Returns:
+        dict: Dictionary of all datasets (in the format of dataframes) from the Family Resources Survey.
+    """
+    dictionary_of_datasets = {}
+    for dataset in frs_datasets:
+        path = f"data/processed/family_resources_survey_{dataset}.csv"
+        dictionary_of_datasets[dataset] = download_obj(
+            BUCKET, path_from=path, download_as="dataframe"
+        )
+    return dictionary_of_datasets
+
+
+def get_individual_dataset(dataset: str) -> pd.DataFrame:
+    """
+    Function to load a specific Family Resources Survey dataset from the UK Data Service.
+
+    Args:
+        dataset (str): Any of the following strings - "accounts", "adult", "assets", "benefits", "benefit_unit", "care", "child", "childcare", "dictionary", "endowment", "ext_child", "frs2223", "gov_pay", "household", "job", "maint", "mort_cont", "mortgage", "odd_job", "owner", "pension_provider","pension","rent_cont", "renter","tables"
+
+    Returns:
+        pd.DataFrame: A dataframe of the specified dataset.
+    """
+    path = f"data/processed/family_resources_survey_{dataset}.csv"
+    return download_obj(BUCKET, path_from=path, download_as="dataframe")
---
+++ afs_mission_goal/pipeline/clean_family_resources_survey.py
@@ -0,0 +1,65 @@
+import pandas as pd
+from afs_mission_goal.getters.uk_data_service.raw.family_resources_survey import *
+from afs_mission_goal.getters.uk_data_service.misc.get_family_resources_survey_dict import (
+    get_family_resources_survey_dict,
+)
+from afs_mission_goal.utils.preprocessing import preprocess_strings
+
+from nesta_ds_utils.loading_saving import S3
+
+frs_columns = get_family_resources_survey_dict()
+
+
+def clean_family_resources_survey(frs_data: pd.DataFrame) -> pd.DataFrame:
+    new_columns = []
+    count = 0
+
+    for col in frs_data.columns:
+        try:
+            new_columns.append(frs_dict[col])
+        except:
+            new_columns.append(col)
+    frs_data.columns = new_columns
+    frs_data.columns = list(preprocess_strings(pd.Series(frs_data.columns)))
+
+    return frs_data
+
+
+if __name__ == "__main__":
+    frs_datasets = [
+        "accounts",
+        "adult",
+        "assets",
+        "benefits",
+        "benefit_unit",
+        "care",
+        "child",
+        "childcare",
+        "dictionary",
+        "endowment",
+        "ext_child",
+        "frs2223",
+        "gov_pay",
+        "household",
+        "job",
+        "maint",
+        "mort_cont",
+        "mortgage",
+        "odd_job",
+        "owner",
+        "pension_provider",
+        "pension",
+        "rent_cont",
+        "renter",
+        "tables",
+    ]
+
+    for dataset in frs_datasets:
+        frs_data = globals()[f"get_{dataset}_data"]()
+        frs_data = clean_family_resources_survey(frs_data)
+        S3.upload_obj(
+            obj=frs_data,
+            bucket="afs-uk-data-service",
+            path_to=f"data/processed/family_resources_survey_{dataset}.csv",
+            kwargs_writing={"index": False},
+        )
